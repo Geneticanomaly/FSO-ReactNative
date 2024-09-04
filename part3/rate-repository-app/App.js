49f9dc20bb2dba66c860592c55ec62b0dcf8d6fe
Geneticanomaly <thomaskaatranen@gmail.com>
2024-09-04 20:45:04 +0300

    Addition of task 10.16: Sign out.

--- part3/rate-repository-app/App.js
+++ part3/rate-repository-app/App.js
@@ -3,15 +3,20 @@ import Main from './src/components/Main';
 import { NativeRouter } from 'react-router-native';
 import { ApolloProvider } from '@apollo/client';
 import createApolloClient from './src/utils/apolloClient';
+import AuthStorage from './src/utils/authStorage';
+import AuthStorageContext from './src/contexts/AuthStorageContext';
 
-const ApolloClient = createApolloClient();
+const authStorage = new AuthStorage();
+const ApolloClient = createApolloClient(authStorage);
 
 export default function App() {
     return (
         <>
             <NativeRouter>
                 <ApolloProvider client={ApolloClient}>
-                    <Main />
+                    <AuthStorageContext.Provider value={authStorage}>
+                        <Main />
+                    </AuthStorageContext.Provider>
                 </ApolloProvider>
             </NativeRouter>
             <StatusBar style="auto" />
--- part3/rate-repository-app/src/components/AppBar.jsx
+++ part3/rate-repository-app/src/components/AppBar.jsx
@@ -3,6 +3,11 @@ import { Link } from 'react-router-native';
 import Constants from 'expo-constants';
 import theme from '../theme';
 import Text from './Text';
+import { useQuery } from '@apollo/client';
+import { ME } from '../graphql/queries';
+import { useNavigate } from 'react-router-native';
+import AuthStorage from '../utils/authStorage';
+import { useApolloClient } from '@apollo/client';
 
 const styles = StyleSheet.create({
     container: {
@@ -20,6 +25,18 @@ const styles = StyleSheet.create({
 });
 
 const AppBar = () => {
+    const navigate = useNavigate();
+    const authStorage = new AuthStorage();
+    const apolloClient = useApolloClient();
+
+    const { data } = useQuery(ME);
+
+    const handleSignOut = () => {
+        authStorage.removeAccessToken();
+        apolloClient.resetStore();
+        navigate('/signIn');
+    };
+
     return (
         <View style={styles.container}>
             <ScrollView horizontal>
@@ -37,6 +54,13 @@ const AppBar = () => {
                         </Text>
                     </Link>
                 </Pressable>
+                {data.me && (
+                    <Pressable onPress={handleSignOut}>
+                        <Text fontSize="heading" style={styles.text}>
+                            Sign out
+                        </Text>
+                    </Pressable>
+                )}
             </ScrollView>
         </View>
     );
